dad5da99302442f8be483cc502c285519b84017e
ldscavo <logan@scavo.dev>
2021-08-05 21:30:08 -0400

    Conditional rendering

--- src/App.fs
+++ src/App.fs
@@ -14,8 +14,8 @@ type Event =
 let init () =
     { Count = 0 }
 
-let update msg state =
-    match msg with
+let update event state =
+    match event with
     | Increment -> { state with Count = state.Count + 1 }
     | Decrement -> { state with Count = state.Count - 1 }
 
@@ -25,11 +25,17 @@ let render state dispatch =
             prop.onClick (fun _ -> dispatch Increment)
             prop.text "Increment"
         ]
+
+        Html.div state.Count
+
         Html.button [
             prop.onClick (fun _ -> dispatch Decrement)
             prop.text "Decrement"
         ]
-        Html.h1 state.Count
+        
+        match state.Count with
+        | 0 -> Html.none
+        | _ -> Html.h1 (if state.Count % 2 = 0 then "Count is even" else "Count is odd")
     ]
 
 Program.mkSimple init update render
