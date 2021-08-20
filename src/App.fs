05848ba118bd69e9d5a523ba29c9c56abf675332
ldscavo <logan@scavo.dev>
2021-08-19 21:32:35 -0400

    async increment

--- src/App.fs
+++ src/App.fs
@@ -5,30 +5,58 @@ open Elmish.React
 open Feliz
 
 type State =
-    { Count: int }
+    { Count: int
+      Loading: bool }
 
 type Event =
     | Increment
     | Decrement
+    | IncrementDelayed
 
 let init () =
-    { Count = 0 }, Cmd.none
+    { Count = 0
+      Loading = false }, Cmd.none
 
 let update event state =
     match event with
-    | Increment -> { state with Count = state.Count + 1 }, Cmd.none
-    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+    | Increment ->
+        { state with
+            Loading = false
+            Count = state.Count + 1 }, Cmd.none
+    | Decrement ->
+        { state with
+            Loading = false
+            Count = state.Count - 1 }, Cmd.none
+    | IncrementDelayed when state.Loading -> state, Cmd.none
+    | IncrementDelayed ->
+        let cmd dispatch = 
+            let delayedDispatch = async {
+                do! Async.Sleep 750
+                dispatch Increment
+            }
+            Async.StartImmediate delayedDispatch
+            
+        { state with Loading = true }, Cmd.ofSub cmd
 
 let render state dispatch =
     Html.div [
-        Html.h1 state.Count
+        if (not state.Loading)
+            then Html.h1 state.Count
+            else Html.h1 "LOADING"
         Html.button [
+            prop.disabled state.Loading
             prop.onClick (fun _ -> dispatch Decrement)
             prop.text "Decrement"
         ]
         Html.button [
+            prop.disabled state.Loading
             prop.onClick (fun _ -> dispatch Increment)
             prop.text "Increment"
+        ]
+        Html.button [
+            prop.disabled state.Loading
+            prop.onClick (fun _ -> dispatch IncrementDelayed)
+            prop.text "Increment, but slowly"
         ]
     ]
 
