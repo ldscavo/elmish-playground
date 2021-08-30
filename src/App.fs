2f37de62ce105f8dec1394e3d9b4c3d341bf0d2b
ldscavo <logan@scavo.dev>
2021-08-29 21:00:34 -0400

    Move TextInput into its own module

--- src/App.fs
+++ src/App.fs
@@ -8,75 +8,38 @@ type Page =
     | Counter
     | TextInput
 
-type TextState =
-    { Text: string
-      IsUpperCase: bool }
-
 type State =
     { CounterState: Counter.State
-      TextState: TextState
+      TextState: TextInput.State
       Page: Page }
-
-type TextEvent =
-    | ChangeText of string
-    | ToggleUpperCase of bool
 
 type Event =
     | CounterEvent of Counter.Event
-    | TextEvent of TextEvent
+    | TextEvent of TextInput.Event
     | PageChanged of Page
-
-let initText () =
-    { Text = "Snog in the Shrubbery!"
-      IsUpperCase = false }
 
 let init () =
     { CounterState = Counter.init ()
-      TextState = initText ()
+      TextState = TextInput.init ()
       Page = Counter }
-
-let updateText event state =
-    match event with
-    | ChangeText text -> { state with Text = text }
-    | ToggleUpperCase isUpper -> { state with IsUpperCase = isUpper }
 
 let update event state =
     match event with
     | PageChanged page ->
         { state with Page = page }
     | CounterEvent evnt ->
-        { state with CounterState = Counter.update evnt state.CounterState }
+        { state with
+            CounterState = Counter.update evnt state.CounterState }
     | TextEvent evnt ->
-        { state with TextState = updateText evnt state.TextState }
-
-let textPage state dispatch =
-    Html.div [        
-        Html.input [
-            prop.valueOrDefault state.Text
-            prop.onChange (ChangeText >> dispatch)
-        ]
-        Html.input [
-            prop.id "uppercase-toggle"
-            prop.type'.checkbox
-            prop.isChecked state.IsUpperCase
-            prop.onChange (ToggleUpperCase >> dispatch)
-        ]
-        Html.label [
-            prop.htmlFor "uppercase-toggle"
-            prop.text "Uppercase?"
-        ]
-        Html.h1
-            (if state.IsUpperCase
-                then state.Text.ToUpper()
-                else state.Text)
-    ]
+        { state with
+            TextState = TextInput.update evnt state.TextState }
 
 let render state dispatch =
     let counterDispatch event =
-        dispatch (Event.CounterEvent event)
+        dispatch (CounterEvent event)
 
     let textDispatch event =
-        dispatch (Event.TextEvent event)
+        dispatch (TextEvent event)
 
     Html.div [
         Html.button [
@@ -90,7 +53,7 @@ let render state dispatch =
         Html.hr []
         match state.Page with
         | Counter -> Counter.render state.CounterState counterDispatch
-        | TextInput -> textPage state.TextState textDispatch
+        | TextInput -> TextInput.render state.TextState textDispatch
     ]
     
 
---
+++ src/TextInput.fs
@@ -0,0 +1,42 @@
+module TextInput
+
+open Feliz
+
+type State =
+    { Text: string
+      IsUpperCase: bool }
+
+type Event =
+    | ChangeText of string
+    | ToggleUpperCase of bool
+
+let init () =
+    { Text = "Snog in the Shrubbery!"
+      IsUpperCase = false }
+  
+let update event state =
+    match event with
+    | ChangeText text -> { state with Text = text }
+    | ToggleUpperCase isUpper -> { state with IsUpperCase = isUpper }
+
+let render state dispatch =
+    Html.div [        
+        Html.input [
+            prop.valueOrDefault state.Text
+            prop.onChange (ChangeText >> dispatch)
+        ]
+        Html.input [
+            prop.id "uppercase-toggle"
+            prop.type'.checkbox
+            prop.isChecked state.IsUpperCase
+            prop.onChange (ToggleUpperCase >> dispatch)
+        ]
+        Html.label [
+            prop.htmlFor "uppercase-toggle"
+            prop.text "Uppercase?"
+        ]
+        Html.h1
+            (if state.IsUpperCase
+                then state.Text.ToUpper()
+                else state.Text)
+    ]
