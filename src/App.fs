6a78a523d3934061e7ca79901459e927e471ef89
ldscavo <logan@scavo.dev>
2021-08-30 21:43:04 -0400

    Add url routing

--- src/App.fs
+++ src/App.fs
@@ -3,6 +3,7 @@ module App
 open Elmish
 open Elmish.React
 open Feliz
+open Feliz.Router
 
 type Page = 
     | Counter
@@ -11,17 +12,22 @@ type Page =
 type State =
     { CounterState: Counter.State
       TextInputState: TextInput.State
-      Page: Page }
+      Page: Page
+      CurrentUrl: string list }
 
 type Event =
     | CounterEvent of Counter.Event
     | TextEvent of TextInput.Event
     | SwitchPage of Page
+    | UrlChanged of string list
+    | NavigateToCounter
+    | NavigateToTextInput
 
 let init () =
     { CounterState = Counter.init ()
       TextInputState = TextInput.init ()
-      Page = Counter }, Cmd.none
+      Page = Counter
+      CurrentUrl = Router.currentUrl() }, Cmd.none
 
 let update event state =
     match event with
@@ -37,20 +43,38 @@ let update event state =
         { state with
             TextInputState = TextInput.update evnt state.TextInputState }, Cmd.none
 
+    | UrlChanged url ->
+        { state with CurrentUrl = url }, Router.Cmd.navigate (List.toArray url)
+
+    | NavigateToCounter ->
+        state, Router.Cmd.navigate "counter"
+
+    | NavigateToTextInput ->
+        state, Router.Cmd.navigate "text-input"
+
 let render state dispatch =
     Html.div [
-        Html.button [
-            prop.text "Show counter page"
-            prop.onClick (fun _ -> dispatch (SwitchPage Counter))
+        Html.a [
+            prop.text "Counter"
+            prop.onClick (fun _ -> dispatch NavigateToCounter)
+            prop.style [ style.margin 10 ]
         ]
-        Html.button [
-            prop.text "Show text page"
-            prop.onClick (fun _ -> dispatch (SwitchPage TextInput))
+        Html.a [
+            prop.text "Text Input"
+            prop.onClick (fun _ -> dispatch NavigateToTextInput)
+            prop.style [ style.margin 10 ]
         ]
-        Html.hr []
-        match state.Page with
-        | Counter -> Counter.render state.CounterState (CounterEvent >> dispatch)
-        | TextInput -> TextInput.render state.TextInputState (TextEvent >> dispatch)
+        Html.hr []        
+        React.router [            
+            router.onUrlChanged (UrlChanged >> dispatch)
+            router.children [ 
+                match state.CurrentUrl with
+                | [] -> Html.h1 "Hello World!"
+                | [ "counter" ] -> Counter.render state.CounterState (CounterEvent >> dispatch)
+                | [ "text-input" ] -> TextInput.render state.TextInputState (TextEvent >> dispatch)
+                | _ -> Html.h1 404
+            ]
+        ]
     ]
     
 
